7f56aa60c821bff90a160c6cff38186b9741ff8c
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-06-16 00:41:39 +0530

    Time: 44 ms (74.2%), Space: 53.9 MB (39.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var fib = function(n) {
-    const cache = {};
+    const cache = {0: 0, 1: 1};
 
     const fun = n => {
         if(n in cache) return cache[n];
@@ -15,6 +15,4 @@ var fib = function(n) {
         return sum;
     }
     return fun(n)
-
-    
 };
