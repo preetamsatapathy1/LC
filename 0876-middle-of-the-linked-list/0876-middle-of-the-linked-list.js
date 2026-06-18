02f706ed3b57a1545870bb12cbd36f49840a66a3
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-06-18 16:03:52 +0530

    Time: 0 ms (100%), Space: 53.8 MB (41.36%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -11,10 +11,11 @@
  */
 var middleNode = function(head) {
     let slow = head, fast = head;
-    while(fast?.next){
-        slow = slow.next;
-        fast = fast.next?.next || null;
+    while(fast != null && fast.next!==null){
+        slow=slow.next;
+        fast = fast.next.next;
     }
-    return slow;
+    return slow
+
     
 };
