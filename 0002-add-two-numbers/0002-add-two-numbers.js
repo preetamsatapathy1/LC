7fe3b99c72673609db56a21f12089e365a4c9d80
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-07-01 01:09:05 +0530

    Time: 4 ms (32.5%), Space: 62.8 MB (8.81%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -13,7 +13,7 @@
 var addTwoNumbers = function(l1, l2) {
     let carry = 0;
     let nh = null, prev = null
-    while(l1 || l2){
+    while(l1 || l2 || carry){
         let n1 = l1?.val || 0;
         let n2 = l2?.val || 0;
         let n3 = (n1 + n2 + carry) % 10;
@@ -27,9 +27,7 @@ var addTwoNumbers = function(l1, l2) {
         l1 = l1?.next;
         l2=l2?.next;
     }
-    if(carry){
-        prev.next = new ListNode(carry);
-    }
+
     return nh;
 
     
