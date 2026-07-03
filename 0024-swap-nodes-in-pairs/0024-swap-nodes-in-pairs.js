b9cc553a253f426024816ef90f52c7d5befce6b9
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-07-03 23:37:04 +0530

    Time: 0 ms (100%), Space: 55.2 MB (5.99%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.js
@@ -10,20 +10,30 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    let curr = head;
-    let sentinel = new ListNode();
-    sentinel.next = head;
-    let prev = sentinel;
-    while(curr && curr.next){
-        let nodeThree = curr.next.next;
-        prev.next = curr.next;
-        curr.next.next = curr;
-        curr.next = nodeThree;
-        prev = curr;
+    // let curr = head;
+    // let sentinel = new ListNode();
+    // sentinel.next = head;
+    // let prev = sentinel;
+    // while(curr && curr.next){
+    //     let nodeThree = curr.next.next;
+    //     prev.next = curr.next;
+    //     curr.next.next = curr;
+    //     curr.next = nodeThree;
+    //     prev = curr;
 
-        curr = curr.next;
-    }
+    //     curr = curr.next;
+    // }
 
-    return sentinel.next;
+    // return sentinel.next;
+
+    if (!head || !head.next) return head;
+
+    let first = head;
+    let second = head.next;
+    let rest = second.next;
+
+    second.next = first;
+    first.next = swapPairs(rest);
+    return second;
     
 };
