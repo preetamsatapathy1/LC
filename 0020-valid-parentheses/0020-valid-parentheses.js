52f6182509756a1aaec0b9c565d897d9130db077
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-07-08 01:15:43 +0530

    Time: 5 ms (33.73%), Space: 58.3 MB (8.4%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -11,6 +11,15 @@ var isValid = function(s) {
 
     const stack = [];
     for(char of s){
+        if(!(char in refMap)) {
+            stack.push(char);
+        } else {
+            const lastVal = stack.pop();
+            if(refMap[char] !== lastVal) {
+                return false
+            }
+        }
+
         if(char in refMap && refMap[char] === stack[stack.length-1]){
             stack.pop()
         }else{
