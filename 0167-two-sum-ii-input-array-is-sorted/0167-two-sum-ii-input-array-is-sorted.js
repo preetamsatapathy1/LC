9444b9112b6daaea687881493f71cd1617562bc3
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-08-01 18:46:26 +0530

    Time: 1 ms (85.13%), Space: 55.3 MB (70.88%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let l=0, r=numbers.length-1;
+    while(l<r){
+        const sum = numbers[l] + numbers[r];
+        if(sum == target) return [l+1, r+1];
+        if(sum < target){
+            l+=1
+        }else{
+            r-=1;
+        }
+    }
+};
