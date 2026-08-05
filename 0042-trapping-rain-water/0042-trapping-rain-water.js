b9583cafe4b763eaa37b9ad73e0f367eecec3e94
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-08-06 01:12:44 +0530

    Time: 6 ms (40.74%), Space: 59.4 MB (36.68%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -3,18 +3,15 @@
  * @return {number}
  */
 var trap = function(height) {
-    let maxL = [height[0]], maxR=new Array(height.length), sum=0;
-    maxR[height.length-1] = height.at(-1);
-    for(let i=1; i<height.length;i++) {
+    let n = height.length;
+    let maxL = [height[0]], maxR=new Array(n), sum=0;
+    maxR[n-1] = height.at(-1);
+    for(let i=1; i<n;i++) {
         maxL[i] = Math.max(height[i], maxL[i-1]);
+        maxR[n-i-1] = Math.max(height[n-i-1], maxR[n-i])
     }
 
-    for(let i=height.length-2; i>=0; i--) {
-        maxR[i] = Math.max(maxR[i+1], height[i])
-    }
-
-
-    for(let i=0; i<height.length;i++){
+    for(let i=0; i<n;i++){
         const level = Math.min(maxL[i], maxR[i]) - height[i];
         sum+=level;
     }
