6c6d276aeb73248923de1c057676ebe4614b8e05
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-08-08 13:20:01 +0530

    Time: 61 ms (22.27%), Space: 61 MB (22.76%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let map = {};
+    let i=0, j=0, max=0;
+    while(i<s.length && j<s.length){
+        const curr = s[j];
+        if(curr in map && map[curr] >=i && map[curr]<=j){
+            i = map[curr] + 1;
+            map[curr] = j
+        }else{
+            const len = j-i+1;
+            max = Math.max(max, len);
+            map[curr] = j
+        }
+        j++;
+
+    }
+    return max
+};
