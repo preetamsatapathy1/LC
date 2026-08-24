ae7b6f7bbf5cf0d136854afea30af5e66ab2490f
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-08-25 00:33:10 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (17.28%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    if(!root) return [];
+
+    const left = inorderTraversal(root.left)
+    const right = inorderTraversal(root.right)
+
+    return [...left, root.val, ...right]
+};
