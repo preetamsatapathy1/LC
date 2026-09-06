b6e08cc8dabedb54c5e438093b99705dcf8f5af4
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-09-06 19:07:41 +0530

    Time: 0 ms (100.00%) | Memory: 59.4 MB (30.28%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.js
@@ -12,19 +12,32 @@
  */
 var levelOrder = function(root) {
     const ans = [];
-    if(!root) return ans;
-    let queue = [root];
-    let front = 0;
-    while(queue.length - front) {
-        const size = queue.length - front;
-        const level = [];
-        for(let i=0; i<size; i++ ){
-            const item = queue[front++];
-            if(item.left) queue.push(item.left);
-            if(item.right) queue.push(item.right);
-            level.push(item.val);
-        }
-        ans.push(level);
+    // if(!root) return ans;
+    // let queue = [root];
+    // let front = 0;
+    // while(queue.length - front) {
+    //     const size = queue.length - front;
+    //     const level = [];
+    //     for(let i=0; i<size; i++ ){
+    //         const item = queue[front++];
+    //         if(item.left) queue.push(item.left);
+    //         if(item.right) queue.push(item.right);
+    //         level.push(item.val);
+    //     }
+    //     ans.push(level);
+    // }
+    // return ans
+
+    const traverse = (root, level=0) => {
+        if(!root) return []
+        if(!ans[level]) ans[level] = [];
+
+        ans[level].push(root.val);
+
+        if(root.left) traverse(root.left, level+1);
+        if(root.right) traverse(root.right, level+1);
     }
-    return ans
+
+    traverse(root, 0);
+    return ans;
 };
