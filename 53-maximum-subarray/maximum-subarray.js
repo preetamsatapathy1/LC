5568a3f594cff0d0fd483653a6efdbfaa068e015
Preetam Satapathy <39155342+preetamsatapathy1@users.noreply.github.com>
2026-09-12 12:57:04 +0530

    Time: 2 ms (70.21%) | Memory: 62.9 MB (92.69%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let ans=-Infinity, curr=0;
+    for(let i=0;i<nums.length;i++){
+        curr+=nums[i];
+        ans = Math.max(ans, curr)
+        if(curr < 0) {
+            curr = 0;
+        }
+    }
+    return ans;
+};
